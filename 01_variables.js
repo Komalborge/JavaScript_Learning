15d158f03667f6ce29697b89befe3dff8d31e289
Komal borge <kborge26@gmail.com>
2022-10-06 15:04:05 +0530

    create 01_Variables
    create 01_Variables

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+var my_apple;//
+my_apple="strawberry";//variable initialization
+var my_name="Komal Borge";//variable declaration and initialization on same line
+my_name="komal santosh borge"
+console.log(my_name);
+console.log(my_apple);
+my_apple="200";
+console.log(my_apple)
