0e8a8bde0322d68c1fbf605aaed64f01982373c6
Komal borge <kborge26@gmail.com>
2022-10-06 15:04:38 +0530

    create01_variable_assign
    create01_variable_assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Komal Santosh Borge"
+console.log("01-",myFullName);
+var myFullName="Komal Borge"
+console.log("02-", myFullName);
+
+var FirstName="Komal"
+var LastName="Borge"
+console.log("First name-",FirstName);
+console.log("Last name-",LastName);
+
+var myLove="I LOVE ONLY JS"
+console.log(myLove);
+
+var FirstName="Komal",LastName="Borge",Age="23",Address="Pune",pincode="411057",Gender="Female",is_married="yes"
+console.log(FirstName,LastName,Age,Address,pincode,Gender,is_married);
+
+
+
+
+
+
