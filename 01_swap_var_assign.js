961aa0151852605f7546a1ed635a7165223b17e3
Komal borge <kborge26@gmail.com>
2022-10-06 15:05:06 +0530

    create 01_swap_var_assign
    create 01_swap_var_assign

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,22 @@
+var Sweety="Sweety";
+var cutie="Cutie";
+console.log("Before swapping","-");
+console.log(Sweety,cutie); 
+
+var temp;
+temp=Sweety;
+Sweety=cutie;
+cutie=temp;
+console.log("After Swapping","-");
+console.log(Sweety,cutie);
+
+console.log("................................................................................")
+var num1=100,num2=200,num3=300;
+console.log("Numbers Before Swapping","-");
+console.log(num1,num2,num3);
+
+console.log("Numbers After Swapping","-");
+num1=num2;
+num2=num3;
+num3=num3-num1;
+console.log(num1,num2,num3);
