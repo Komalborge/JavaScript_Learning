bf3452ab2be081060e1db5b18f8a32b2d4caf5ad
Komal borge <kborge26@gmail.com>
2022-10-07 18:16:14 +0530

    create 02_datatype_assign
    create 02_datatype_assign

---
+++ 02_datatype_assign.js
@@ -0,0 +1,22 @@
+console.log("=======number=======");
+var PIN=411056
+console.log("PIN:411056","type:",typeof PIN);
+
+console.log("=======string=======");
+var name="komal"
+console.log("name:komal","type:",typeof name);
+
+console.log("======Boolean======");
+var is_married=true
+console.log("is_married","type:",typeof is_married );
+
+//check value null or undefined//
+var k_name;
+console.log("================");
+var k_name=null;
+console.log(typeof k_name);
+function functionName(){
+
+}
+var object=functionName()
+console.log(object);
