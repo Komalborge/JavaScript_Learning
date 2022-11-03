d36e9fcea49adcd57b4c6cf844e6502965da6a50
Komal borge <kborge26@gmail.com>
2022-11-03 23:31:18 +0530

    Array Assignment
    Array Assignment

---
+++ 10_array_assignments.js
@@ -0,0 +1,9 @@
+console.error("----Assignment-02---");
+console.log("=========================Remove the Duplicate names ==================");
+let arrayofNames=["kamat","Menon","Nashpati","Taimur","Menon","kamat","Andy","Taimur"]
+let array=[...new Set(arrayofNames)]
+console.log(array);
+
+console.log("==========================Count of duplicate Elements========================");
+console.log(arrayofNames.length);
+console.log(arrayofNames);
